b8ae630527ef70e787c22785083bcd00364303f6
harel <harel@200>
2024-10-16 12:12:58 +0300

    Created SevenBoomMultiThreaded class

---
+++ src/SevenBoomMultiThreaded.java
@@ -0,0 +1,22 @@
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+public class SevenBoomMultiThreaded {
+    private static final int THREAD_COUNT = 4;
+    private final Object SEVEN_BOOM_LOCK = new Object();
+    private final int maxInput;
+
+    public SevenBoomMultiThreaded(int maxInput) {
+        this.maxInput = maxInput;
+    }
+
+    public void run() {
+        Predicate<Integer> isSevenBoom = n -> n % 7 == 0 || Integer.toString(n).contains("7");
+        var inputs = IntStream.range(1, maxInput + 1).iterator();
+        var boomFormatter = new BoomFormatter(isSevenBoom);
+
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            new SevenBoomThread(SEVEN_BOOM_LOCK, inputs, boomFormatter).start();
+        }
+    }
+}
